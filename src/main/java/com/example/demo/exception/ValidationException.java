e6b44c41fbfb8f38cbb47a6977e8979ea68a2993
pariwat.vi <aigbolo@gmail.com>
2019-08-16 13:47:57 +0700

    update validation exception

--- src/main/java/com/example/demo/exception/ValidationException.java
+++ src/main/java/com/example/demo/exception/ValidationException.java
@@ -9,11 +9,6 @@ public class ValidationException extends Exception {
 
 	private String code;
 
-	public ValidationException(String theCode, String theMessage) {
-		super(theMessage);
-		this.setCode(theCode);
-	}
-
 	public ValidationException(ExceptionMessages exceptionMessages) {
 		super(exceptionMessages.message);
 		this.setCode(exceptionMessages.code);
